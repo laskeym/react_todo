f7cad81e831e54223c089a8db16ddbd0a76e4b8e
Mark Laskey <laskey.mark@hotmail.com>
2018-12-29 16:49:16 -0500

    Updates

---
+++ precache-manifest.19980f75c4709057636cccdf7f9ef035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92900329656bd339c499",
+    "url": "/react_todo/static/js/runtime~main.92900329.js"
+  },
+  {
+    "revision": "911def64baa75d5bd870",
+    "url": "/react_todo/static/js/main.911def64.chunk.js"
+  },
+  {
+    "revision": "5d53f20d9f8f9429ae33",
+    "url": "/react_todo/static/js/1.5d53f20d.chunk.js"
+  },
+  {
+    "revision": "911def64baa75d5bd870",
+    "url": "/react_todo/static/css/main.048be07e.chunk.css"
+  },
+  {
+    "revision": "5d53f20d9f8f9429ae33",
+    "url": "/react_todo/static/css/1.6cf28de8.chunk.css"
+  },
+  {
+    "revision": "220a89e96e74e25a1d746a0040786519",
+    "url": "/react_todo/index.html"
+  }
+];
